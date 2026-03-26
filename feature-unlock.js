05a29ea1befdb6bb0cc20ff6d0acb575fea0c92f
iwanderwithu <vadezkam@gmail.com>
2026-03-26 09:57:31 -0400

    Add feature-unlock.js to unlock Pro features for testing.

---
+++ feature-unlock.js
@@ -0,0 +1,7 @@
+// Code to unlock Pro features for testing
+function unlockProFeatures() {
+    // Logic to unlock Pro features
+    console.log('Pro features unlocked for testing.');
+}
+
+unlockProFeatures();
